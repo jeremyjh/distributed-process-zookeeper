7aaf59889f6ed9b422decc92f595c1f6a1eb6f0c
Jeremy Huffman <jeremy@jeremyhuffman.com>
2015-01-15 08:48:00 -0500

    create initial nodes, registration

--- src/Control/Distributed/Process/Zookeeper.hs
+++ src/Control/Distributed/Process/Zookeeper.hs
@@ -6,11 +6,13 @@ module Control.Distributed.Process.Zookeeper
    , registerZK
    ) where
 
-import Control.Distributed.Process
+import Control.Distributed.Process hiding (proxy)
 import Control.Distributed.Process.Serializable
 import Control.Distributed.Process.MonadBaseControl ()
 import Database.Zookeeper.Lifted
-import Control.Monad (void)
+import Control.Monad (void, join)
+import Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
+import Control.Exception (throwIO)
 import Data.Binary (Binary, encode)
 import Data.Typeable (Typeable)
 import qualified Data.ByteString.Lazy as BL
@@ -18,6 +20,7 @@ import qualified Data.ByteString as BS
 import GHC.Generics (Generic)
 
 data Command = Register String ProcessId (SendPort (Either String ()))
+             | NodeList [String]
              | Exit
     deriving (Show, Typeable, Generic)
 
@@ -28,36 +31,53 @@ instance Binary Command
 zkController :: String -- ^ The Zookeeper endpoint, as in 'Base.withZookeeper'
              -> Process ()
 zkController services =
-   void $ spawnLocal $
+ do proxy <- spawnProxy
+    zkpid <- spawnLocal $
      do pid <- getSelfPid
-        withZookeeper services 1000 (Just $ watcher pid) Nothing $ \rzh ->
-            let loop =
-                 do command <- expect
+        withZookeeper services 1000 (Just $ watcher proxy pid) Nothing $ \rzh ->
+            let loop st@(_, nodes) =
+                 do liftIO $ print ("nodes", nodes)
+                    command <- expect
                     case command of
                         Exit -> return ()
-                        _ -> handle rzh command >> loop
-            in loop
+                        _ -> handle st command >>= loop
+            in loop (rzh, [])
+    register controller zkpid
   where
-    handle rzh (Register name rpid reply) =
-     do result <- create rzh (name </> pidS rpid) Nothing OpenAclUnsafe [Ephemeral]
+    handle st@(rzh, _) (Register name rpid reply) =
+     do result <- create rzh (rootNode </> name) (pidB rpid) OpenAclUnsafe [Ephemeral]
         case result of
             Right _ -> sendChan reply (Right ())
             Left reason -> sendChan reply (Left $ show reason)
+        return st
 
-    handle _ Exit = return ()
+    handle (rzh, _) (NodeList nodes) =
+        return (rzh, nodes)
 
-    watcher pid rzh SessionEvent ConnectedState _ =
-      void $ do void $ create rzh (pidS pid) Nothing OpenAclUnsafe [Ephemeral]
-                Right children <- getChildren rzh "/" Nothing
-                mapM putStrLn children
+    handle st Exit = return st
 
-    watcher _ _ _ _ _ = return ()
+    watcher proxy pid rzh SessionEvent ConnectedState _ =
+      void $ do
+                create rzh rootNode Nothing OpenAclUnsafe [] >>= assertNode rootNode
+                create rzh servicesNode Nothing OpenAclUnsafe [] >>= assertNode servicesNode
+                create rzh controllersNode Nothing OpenAclUnsafe [] >>= assertNode controllersNode
+                Right _ <- create rzh (controllersNode </> pretty pid)
+                                      (pidB pid) OpenAclUnsafe [Ephemeral]
+                Right children <- getChildren rzh controllersNode Nothing --(rootP </> "nodes") Nothing
+                proxy $ castZK (NodeList children)
 
-    serviceP name = rootP </> "services" </> name
-    rootP = "/distributed-process"
+    watcher _ _ _ _ _ _ = return ()
+
+    servicesNode = rootNode </> "services"
+    controllersNode = rootNode </> "controllers"
+    rootNode = "/distributed-process"
     l </> r = l ++ "/" ++ r
-    pidS pid = "/" ++ drop 6 (show pid)
+    pretty pid = drop 6 (show pid)
     pidB pid = Just . BS.concat . BL.toChunks $ encode pid
+
+    assertNode _ (Right _) = return ()
+    assertNode _ (Left NodeExistsError) = return ()
+    assertNode name (Left _) = throwIO (userError $ "Fatal: could not create node: " ++ name)
 
 registerZK :: String -> ProcessId -> Process (Either String ())
 registerZK name rpid =
@@ -75,3 +95,16 @@ callZK command =
        result <- receiveChan replyCh
        unlink pid
        return result
+
+castZK :: Command -> Process ()
+castZK command =
+    do Just pid <- whereis controller
+       send pid command
+
+spawnProxy :: Process (Process () -> IO ())
+spawnProxy =
+ do mv <- liftIO newEmptyMVar
+    void $ spawnLocal $
+        let loop = join (liftIO $ takeMVar mv) >> loop
+        in loop
+    return (putMVar mv)
