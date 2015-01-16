cac77c34e71fa2fa1f1d8fa0455a92500ba0cbcf
Jeremy Huffman <jeremy@jeremyhuffman.com>
2015-01-15 22:08:42 -0500

    delete registrations on process exit

--- src/Control/Distributed/Process/Zookeeper.hs
+++ src/Control/Distributed/Process/Zookeeper.hs
@@ -1,16 +1,15 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Control.Distributed.Process.Zookeeper
    ( zkController
    , registerZK
    ) where
 
-import Control.Distributed.Process hiding (proxy)
-import Control.Distributed.Process.Serializable
-import Control.Distributed.Process.MonadBaseControl ()
-import Database.Zookeeper.Lifted
-import Control.Monad (void, join)
+import Database.Zookeeper.Lifted hiding (State)
+
+import Control.Monad (void, join, forM_)
 import Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
 import Control.Exception (throwIO)
 import Data.Binary (Binary, encode)
@@ -18,11 +17,31 @@ import Data.Typeable (Typeable)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as BS
 import GHC.Generics (Generic)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Monoid (mempty)
+import Data.Maybe (fromMaybe)
+
+import Control.Distributed.Process hiding (proxy)
+import Control.Distributed.Process.Serializable
+import Control.Distributed.Process.MonadBaseControl ()
+
+
 
 data Command = Register String ProcessId (SendPort (Either String ()))
              | NodeList [String]
              | Exit
     deriving (Show, Typeable, Generic)
+
+data State = State
+    {
+      conn :: Zookeeper
+    , nodes :: [String]
+    -- service nodes to remove each pid from when it exits
+    , monPids :: Map ProcessId [String]
+    }
+instance Show State where
+    show State{..} = show ("nodes", nodes, "monPids", monPids)
 
 instance Binary Command
 
@@ -35,24 +54,44 @@ zkController services =
     zkpid <- spawnLocal $
      do pid <- getSelfPid
         withZookeeper services 1000 (Just $ watcher proxy pid) Nothing $ \rzh ->
-            let loop st@(_, nodes) =
-                 do liftIO $ print ("nodes", nodes)
-                    command <- expect
-                    case command of
-                        Exit -> return ()
-                        _ -> handle st command >>= loop
-            in loop (rzh, [])
+            let loop st@State{..} =
+                    let recvCmd = match $ \command -> case command of
+                                            Exit -> terminate --TODO: this could be better
+                                            _ -> handle st command
+                        recvMon = match $ \(ProcessMonitorNotification _ dead _) ->
+                                            reap st dead
+                    in say (show st) >> receiveWait [recvCmd, recvMon] >>= loop
+            in loop (State rzh [] mempty)
     register controller zkpid
   where
-    handle st@(rzh, _) (Register name rpid reply) =
-     do result <- create rzh (rootNode </> name) (pidB rpid) OpenAclUnsafe [Ephemeral]
+    reap st@State{..} pid =
+        let names = fromMaybe [] (Map.lookup pid monPids)
+        in do forM_ names $ \name ->
+               do let node = servicesNode </> name </> pretty pid
+                  result <- delete conn node Nothing
+                  case result of
+                    Left reason -> say $ "Error: "
+                                         ++ show reason
+                                         ++ " - failed to delete "
+                                         ++ node
+                    _ -> say $ "Deleted " ++ node
+              return st{monPids = Map.delete pid monPids}
+    handle st@State{..} (Register name rpid reply) =
+     do let node = servicesNode </> name </> pretty rpid
+        create conn (servicesNode </> name)
+                    Nothing OpenAclUnsafe [] >>= assertNode name
+        result <- create conn node (pidB rpid)
+                         OpenAclUnsafe [Ephemeral]
         case result of
-            Right _ -> sendChan reply (Right ())
+            Right _ ->
+             do say $ "Registered " ++ node
+                sendChan reply (Right ())
             Left reason -> sendChan reply (Left $ show reason)
-        return st
 
-    handle (rzh, _) (NodeList nodes) =
-        return (rzh, nodes)
+        void $ monitor rpid
+        return st{monPids = Map.insertWith (++) rpid [name] monPids}
+
+    handle st (NodeList n) = return st{nodes = n}
 
     handle st Exit = return st
 
@@ -63,7 +102,7 @@ zkController services =
                 create rzh controllersNode Nothing OpenAclUnsafe [] >>= assertNode controllersNode
                 Right _ <- create rzh (controllersNode </> pretty pid)
                                       (pidB pid) OpenAclUnsafe [Ephemeral]
-                Right children <- getChildren rzh controllersNode Nothing --(rootP </> "nodes") Nothing
+                Right children <- getChildren rzh controllersNode Nothing
                 proxy $ castZK (NodeList children)
 
     watcher _ _ _ _ _ _ = return ()
@@ -77,7 +116,8 @@ zkController services =
 
     assertNode _ (Right _) = return ()
     assertNode _ (Left NodeExistsError) = return ()
-    assertNode name (Left _) = throwIO (userError $ "Fatal: could not create node: " ++ name)
+    assertNode name (Left _) = liftIO $
+        throwIO (userError $ "Fatal: could not create node: " ++ name)
 
 registerZK :: String -> ProcessId -> Process (Either String ())
 registerZK name rpid =
--- src/Database/Zookeeper/Lifted.hs
+++ src/Database/Zookeeper/Lifted.hs
@@ -2,9 +2,10 @@ module Database.Zookeeper.Lifted
     ( module Database.Zookeeper
     , withZookeeper
     , create
+    , delete
     ) where
 
-import Database.Zookeeper hiding (create,withZookeeper)
+import Database.Zookeeper hiding (create, delete, withZookeeper)
 import qualified Database.Zookeeper as Base
 import Control.Monad.Trans.Control (control)
 import Control.Monad.Base (liftBase)
@@ -13,3 +14,5 @@ withZookeeper s t w c zma = control $ \runInIO ->
     Base.withZookeeper s t w c (runInIO . zma)
 
 create z n d a = liftBase . Base.create z n d a
+
+delete z n = liftBase . Base.delete z n
